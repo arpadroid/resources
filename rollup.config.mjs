cdb794924826560b4cbb6602df0c7640858ab3ce
Andres Vaquero <web.andresvaquero@gmail.com>
2024-08-20 21:38:04 +1000

    adding resources

--- rollup.config.mjs
+++ rollup.config.mjs
@@ -1,3 +1,5 @@
-import { getBuild } from '@arpadroid/arpadroid/src/rollup/builds/rollup-builds.mjs';
-const { build } = getBuild('resources', 'uiComponent');
+import { getBuild, isSlim } from '@arpadroid/arpadroid/src/rollup/builds/rollup-builds.mjs';
+const { build } = getBuild('resources', 'uiComponent', {
+    external : isSlim() ? ['lists', 'application'] : ['application']
+});
 export default build;
